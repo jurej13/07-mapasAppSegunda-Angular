d2d7e4724c23797594b8e94760eaabbcc2a46571
Javier Jure <jjure13@gmail.com>
2022-05-16 16:34:15 -0300

    Mapas app terminada

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken:'pk.eyJ1IjoianVyZWoxMyIsImEiOiJjbDM4eDN2NXEwMW1iM2JvM2FjeWw3anJwIn0.yaYCAHHToneGeqjCjPYKhw'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxToken:'pk.eyJ1IjoianVyZWoxMyIsImEiOiJjbDM4eDN2NXEwMW1iM2JvM2FjeWw3anJwIn0.yaYCAHHToneGeqjCjPYKhw'
 };
 
 /*
